f065bf5f0542f0cd14d57108bd90ae4b95a8fcb7
Tara Liang <TaraLiang93@gmail.com>
2018-01-18 01:22:37 -0500

    this should be it done?

--- src/Connected.java
+++ src/Connected.java
@@ -17,45 +17,53 @@ public class Connected {
             BufferedReader f = new BufferedReader(new FileReader(args[0]));
             Map<String, City> cityList = new HashMap<String, City>(); //a list of all the city in this world
 
-            String line;
-            while((line = f.readLine())!= null){
-                addCitiesnConnection(cityList,line);
-            }
-            //all connection are made here
-            if((cityList.containsKey(args[1])==false)||(cityList.containsKey(args[2]))==false){
-                //if the orgin or dest does not exist in the city list return no and end program
+            String line = f.readLine();
+            if(line == null){
+                //empty file
                 System.out.println("No");
                 System.exit(1);
+            }
+            addCitiesnConnection(cityList,line);
+            while((line = f.readLine())!= null){//keep reading untill the end
+                addCitiesnConnection(cityList,line);
+            }
+            //all connections are made by this point
+            if((cityList.containsKey(args[1])==false)||(cityList.containsKey(args[2]))==false){
+                //if the orgin or dest city does not exist in the city list return no and end program
+                System.out.println("No");
+                System.exit(0);
             }
             if(args[1].equalsIgnoreCase(args[2])){
                 //origin and dest are the same city
                 System.out.println("Yes");
-                System.exit(1);
+                System.exit(0);
             }
 
-            ArrayList<String> visited = new ArrayList<String>();
-            visited.add(args[1]);
-            City origin = cityList.get(args[1]);
-            if(origin.getConnection().containsKey(args[2])){
-                System.out.println("Yes");
-                System.exit(1);
-            }
+            ArrayList<String> visited = new ArrayList<String>();//record the list of visited cities
+            City origin = cityList.get(args[1].trim().toLowerCase());
+            City dest = cityList.get(args[2].trim().toLowerCase());
 
-                System.out.println(visited.toString());
+            searchConnection(origin, dest, visited);
+            //by the time the code reach here there are no direct connection between two cities, and return no
+            System.out.println("No");
+            System.exit(0);
+
         }catch(IOException e){//no such file
             System.out.println("No");
             e.printStackTrace();
+            System.exit(1);
         }catch(ArrayIndexOutOfBoundsException e){//not enough input args
             System.out.println("Missing input args\nNo");
-            e.printStackTrace();
+            e.printStackTrace();            
+            System.exit(1);
         }
 
     }
 
     public static void addCitiesnConnection(Map<String, City> cityList, String input){
         try{
-            String[] cities = input.trim().split("\\s*,\\s*");
-            if(cities.length>2){ //if there is a mistake in tokenizing the line
+            String[] cities = input.trim().toLowerCase().split("\\s*,\\s*");// remove all space and make all letter into lowercase
+            if(cities.length!=2){ //if there extra cities or missing cities
                 throw new errorLoadingCity(input);
             }
             City temp;
@@ -67,9 +75,6 @@ public class Connected {
                 cityList.put(temp.getCityName(), temp);
             }
             if(cityList.containsKey(cities[1]) == false){
-                //sec city in the line is not in the list of existing cities.
-                // Create the obj for city and add to list
-                //if the city exist in the list then don't add to list
                 temp = new City(cities[1]);
                 cityList.put(temp.getCityName(), temp);
             }
@@ -78,11 +83,30 @@ public class Connected {
 
         }catch(errorLoadingCity e){
             System.out.println(e.getMessage());
+            System.out.println("No");
+            System.exit(1);
+
         }
     }
 
     public static void addConnection(Map<String, City> cityList, String[] cities){
         cityList.get(cities[0]).addConnection(cityList.get(cities[1]));//get the first city and add the connection to the sec city from input
         cityList.get(cities[1]).addConnection(cityList.get(cities[0]));
+    }
+
+    public static void searchConnection(City ptr, City dest, ArrayList<String> v){
+        v.add(ptr.getCityName());//mark current city as visited
+        if(ptr.getConnection().containsKey(dest.getCityName())){
+            //one of the connected city is the dest search ends in success
+            System.out.println("Yes");
+            System.exit(0);
+        }
+        for(City adj : ptr.getConnection().values()){
+            if(v.contains(adj.getCityName())==false){
+                searchConnection(adj, dest, v);
+            }
+        }
+
+
     }
 }
